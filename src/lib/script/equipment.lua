b28b24f837c4e226e688cde301fed8dd032d684b
ldinc <drogunov.igor@gmail.com>
2024-11-14 02:32:18 +0300

    v0.2.3

--- src/lib/script/equipment.lua
+++ src/lib/script/equipment.lua
@@ -4,9 +4,15 @@ require('lib.script.settings')
 ---@param player LuaPlayer
 function ldinc_starting_equipment.fn.check_starting_equipment(player)
 	local items = ldinc_starting_equipment.fn.get_default()
+	local additional_len = 0
 
-	if not ldinc_starting_equipment.fn.settings_ignore_others() and #ldinc_starting_equipment.additional > 0 then
+	for _ in pairs(ldinc_starting_equipment.additional) do
+		additional_len = additional_len + 1
+	end
+
+	if not ldinc_starting_equipment.fn.settings_ignore_others() and additional_len > 0 then
 		if not ldinc_starting_equipment.fn.settings_append_default_to_others() then
+			log("no default")
 			items = {}
 		end
 
